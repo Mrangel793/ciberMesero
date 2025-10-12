41fe042277676b89d5deda0b05a0a2b63633cea7
Marly Rangel <129441027+Mrangel793@users.noreply.github.com>
2025-10-12 17:47:07 -0500

    ajste

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="w-full min-h-screen">
-<<<<<<< HEAD
     <router-view></router-view>
-=======
-    <RouterView />
->>>>>>> 51b9f1f598dd0e5ec16babdbeff1295621c01ec4
   </div>
 </template>
 
