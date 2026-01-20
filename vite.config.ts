4316fd0598b7c1aeff38f5fc2c6e89ab43722172
David Carrillo <dake92083@gmail.com>
2026-01-20 09:19:02 -0500

    build: comment out vite-plugin-vue-devtools in vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
@@ -16,3 +16,4 @@ export default defineConfig({
     },
   },
 })
+
